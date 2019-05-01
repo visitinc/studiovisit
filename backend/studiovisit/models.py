7947dd3cd27b1b121092df52c4a3da7f4283ffb9
Patrick Steadman <ptsteadman@gmail.com>
2019-04-30 20:21:59 -0400

    add more database fields

--- backend/studiovisit/models.py
+++ backend/studiovisit/models.py
@@ -4,4 +4,32 @@ from django.db import models
 class Field(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=500)
+    def __str__(self):
+        return self.name
 
+class Institution(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+    location = models.ForeignKey('Location', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+    # latlon
+    def __str__(self):
+        return self.name
+
+class Discourse(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+    def __str__(self):
+        return self.name
+
+class Discipline(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+    def __str__(self):
+        return self.name
